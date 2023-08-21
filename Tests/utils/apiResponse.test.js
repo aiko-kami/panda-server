5580262790e8fc120b71923fa71f95e3f49a0cfc
Adrian-loup <adrianloup@gmail.com>
2023-08-21 23:27:12 +0200

    removed last occurences of validationErrorWithData

--- Tests/utils/apiResponse.test.js
+++ Tests/utils/apiResponse.test.js
@@ -4,7 +4,6 @@ const {
 	successResponse,
 	successResponseWithData,
 	clientErrorResponse,
-	validationErrorWithData,
 	serverErrorResponse,
 	notFoundResponse,
 	unauthorizedResponse,
--- utils/apiResponse.js
+++ utils/apiResponse.js
@@ -57,7 +57,6 @@ module.exports = {
 	successResponse,
 	successResponseWithData,
 	clientErrorResponse,
-	validationErrorWithData,
 	serverErrorResponse,
 	notFoundResponse,
 	unauthorizedResponse,
