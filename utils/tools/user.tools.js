775154095ff96773c2e393d3a8445e24371618cf
Adrian-loup <adrianloup@gmail.com>
2023-09-17 09:46:21 +0200

    fixed update user

--- utils/tools/user.tools.js
+++ utils/tools/user.tools.js
@@ -3,56 +3,56 @@ const filterFieldsToUpdate = (userData) => {
 
 	if (userData.email) {
 		if (userData.email === "@--empty--string") {
-			userData.email = "";
+			filteredInputs.email = "";
 		} else {
 			filteredInputs.email = userData.email;
 		}
 	}
 	if (userData.profilePicture) {
 		if (userData.profilePicture === "@--empty--string") {
-			userData.profilePicture = "";
+			filteredInputs.profilePicture = "";
 		} else {
 			filteredInputs.profilePicture = userData.profilePicture;
 		}
 	}
 	if (userData.locationCountry) {
 		if (userData.locationCountry === "@--empty--string") {
-			userData.locationCountry = "";
+			filteredInputs.locationCountry = "";
 		} else {
 			filteredInputs.locationCountry = userData.locationCountry;
 		}
 	}
 	if (userData.locationCity) {
 		if (userData.locationCity === "@--empty--string") {
-			userData.locationCity = "";
+			filteredInputs.locationCity = "";
 		} else {
 			filteredInputs.locationCity = userData.locationCity;
 		}
 	}
 	if (userData.company) {
 		if (userData.company === "@--empty--string") {
-			userData.company = "";
+			filteredInputs.company = "";
 		} else {
 			filteredInputs.company = userData.company;
 		}
 	}
 	if (userData.description) {
 		if (userData.description === "@--empty--string") {
-			userData.description = "";
+			filteredInputs.description = "";
 		} else {
 			filteredInputs.description = userData.description;
 		}
 	}
 	if (userData.bio) {
 		if (userData.bio === "@--empty--string") {
-			userData.bio = "";
+			filteredInputs.bio = "";
 		} else {
 			filteredInputs.bio = userData.bio;
 		}
 	}
 	if (userData.website) {
 		if (userData.website === "@--empty--string") {
-			userData.website = "";
+			filteredInputs.website = "";
 		} else {
 			filteredInputs.website = userData.website;
 		}
@@ -64,6 +64,7 @@ const filterFieldsToUpdate = (userData) => {
 			filteredInputs.languages = userData.languages;
 		}
 	}
+	console.log("🚀 ~ filterFieldsToUpdate ~ filteredInputs:", filteredInputs);
 
 	return filteredInputs;
 };
