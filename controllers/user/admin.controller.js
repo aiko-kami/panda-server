97656c9b5c18ee94805edc416599e711b770aedb
Adrian-loup <adrianloup@gmail.com>
2023-10-29 20:44:34 +0100

    started admin creation

---
+++ controllers/user/admin.controller.js
@@ -0,0 +1,114 @@
+const { apiResponse, userValidation, authValidation, userTools } = require("../../utils");
+const { userService } = require("../../services");
+
+const retrieveMyUserData = async (req, res) => {
+	try {
+		const userId = req.userId;
+
+		const userData = await userService.retrieveUserById(userId, "-_id username email createdAt location company description bio languages website profilePicture");
+
+		if (userData.status !== "success") {
+			return apiResponse.serverErrorResponse(res, userData.message);
+		}
+
+		return apiResponse.successResponseWithData(res, userData.message, userData.user);
+	} catch (error) {
+		// Throw error in json response with status 500.
+		return apiResponse.serverErrorResponse(res, error.message);
+	}
+};
+
+/**
+ * Update User Controller
+ * This controller handles the update of user personal information.
+ * @param {Object} req - The HTTP request object.
+ * @param {Object} res - The HTTP response object.
+ * @returns {Object} - The response containing the updated user or an error message.
+ */
+const updateUser = async (req, res) => {
+	try {
+		const userId = req.userId;
+
+		//Retrieve and initialize user data
+		const updatedUserInputs = {
+			email: req.body.userNewData.email || "",
+			profilePicture: req.body.userNewData.profilePicture || "",
+			locationCountry: req.body.userNewData.locationCountry || "",
+			locationCity: req.body.userNewData.locationCity || "",
+			company: req.body.userNewData.company || "",
+			description: req.body.userNewData.description || "",
+			bio: req.body.userNewData.bio || "",
+			languages: req.body.userNewData.languages || [],
+			website: req.body.userNewData.website || "",
+		};
+
+		// Validate input data for updating a user
+		const validationResult = userValidation.validateUpdatedUserInputs(updatedUserInputs);
+		if (validationResult.status !== "success") {
+			return apiResponse.clientErrorResponse(res, validationResult.message);
+		}
+
+		// Filter on the fields that the user wants to update
+		const filterUserInputs = userTools.filterFieldsToUpdate(updatedUserInputs);
+
+		//Verify that the email (if modified) is available
+		if (filterUserInputs.email) {
+			const emailVerification = await userService.verifyEmailAvailability(filterUserInputs.email);
+			if (emailVerification.status !== "success") {
+				return apiResponse.serverErrorResponse(res, emailVerification.message);
+			}
+		}
+
+		// Update the user in the database
+		const updateUserResult = await userService.updateUser(userId, filterUserInputs);
+
+		// Check the result of the update operation
+		if (updateUserResult.status !== "success") {
+			return apiResponse.serverErrorResponse(res, updateUserResult.message);
+		}
+
+		return apiResponse.successResponse(res, updateUserResult.message);
+	} catch (error) {
+		return apiResponse.serverErrorResponse(res, error.message);
+	}
+};
+
+/**
+ * Change User's password Controller
+ * This controller handles the update of user's password.
+ * @param {Object} req - The HTTP request object.
+ * @param {Object} res - The HTTP response object.
+ * @returns {Object} - The response containing the updated password status or an error message.
+ */
+const updateUserPassword = async (req, res) => {
+	try {
+		const userId = req.userId;
+
+		const { newPassword = "", confirmNewPassword = "" } = req.body;
+
+		// Validate input data
+		const validationResult = authValidation.validatePassword(newPassword, confirmNewPassword);
+		if (validationResult.status !== "success") {
+			return apiResponse.clientErrorResponse(res, validationResult.message);
+		}
+
+		// Update the user's password in the database
+		const updatePasswordResult = await userService.updateUserPassword(userId, newPassword);
+
+		// Check the result of the update operation
+		if (updatePasswordResult.status !== "success") {
+			return apiResponse.serverErrorResponse(res, updatePasswordResult.message);
+		}
+
+		return apiResponse.successResponse(res, updatePasswordResult.message);
+	} catch (error) {
+		return apiResponse.serverErrorResponse(res, error.message);
+	}
+};
+
+module.exports = {
+	retrieveMyUserData,
+	updateUser,
+	retrieveNewUsers,
+	updateUserPassword,
+};
---
+++ models/admin.model.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const { dbConnectionAdmin } = require("../config/db.config");
+const v4 = require("uuid").v4;
+const { DateTime } = require("luxon");
+
+const adminUserSchema = new mongoose.Schema(
+	{
+		adminUserId: { type: String, default: v4, required: true, unique: true },
+		username: { type: String, required: true, unique: true, trim: true },
+		usernameCapitalized: { type: String, required: true, unique: true, trim: true },
+		email: { type: String, required: true, unique: true, trim: true, lowercase: true },
+		password: { type: String, required: true, trim: true },
+		createdAt: { type: Date, default: DateTime.now().toHTTP(), required: true },
+		lastConnection: { type: Date },
+		profilePicture: { type: String },
+		location: { city: { type: String }, country: { type: String } },
+		description: { type: String },
+		bio: { type: String },
+		languages: [{ type: String }],
+		website: { type: String },
+	},
+	{
+		collection: "admins",
+		timestamps: true, // Automatically add createdAt and updatedAt timestamps
+	}
+);
+
+/*
+// Custom validator to ensure at least one talent is present
+userSchema.path("talents").validate(function (value) {
+	return value && value.length > 0;
+}, "At least one talent is required.");
+*/
+
+const AdminUser = dbConnectionAdmin.model("AdminUser", userSchema);
+
+module.exports = AdminUser;
