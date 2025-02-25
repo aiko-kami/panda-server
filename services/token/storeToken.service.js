7378b7781916790e666cb634f09f76b9d91e19cf
Adrian-loup <adrianloup@gmail.com>
2025-02-25 22:27:24 +0100

    httpOnly: false

--- services/token/storeToken.service.js
+++ services/token/storeToken.service.js
@@ -7,7 +7,7 @@ const setTokensInCookies = (res, accessToken, refreshToken) => {
 	const isProduction = process.env.NODE_ENV === "production";
 
 	res.cookie("access_token", accessToken, {
-		httpOnly: true, // Set httpOnly to true in production and to false in development
+		httpOnly: false, // Set httpOnly to true in production and to false in development
 		secure: !isDevelopment, // Set secure to true in production and to false in development
 		sameSite: isDevelopment ? "Lax" : "None",
 		partitioned: !isDevelopment,
@@ -16,7 +16,7 @@ const setTokensInCookies = (res, accessToken, refreshToken) => {
 	});
 
 	res.cookie("refresh_token", refreshToken, {
-		httpOnly: true, // Set httpOnly to true in production and to false in development
+		httpOnly: false, // Set httpOnly to true in production and to false in development
 		secure: !isDevelopment, // Set secure to true in production and to false in development
 		sameSite: isDevelopment ? "Lax" : "None",
 		partitioned: !isDevelopment,
