0cfec02154ee24d716c1428729cb47b6581f7531
Adrian-loup <adrianloup@gmail.com>
2025-05-17 10:28:20 +0200

    added domain in cookie

--- services/token/storeToken.service.js
+++ services/token/storeToken.service.js
@@ -9,8 +9,9 @@ const setTokensInCookies = (res, accessToken, refreshToken) => {
 	res.cookie("access_token", accessToken, {
 		httpOnly: true, // Set httpOnly to true in production and to false in development
 		secure: !isDevelopment, // Set secure to true in production and to false in development
+		domain: ".neutroneer.com",
 		sameSite: isDevelopment ? "Lax" : "Strict",
-		Path: "/",
+		path: "/",
 		maxAge: 1000 * parseInt(process.env.ACCESS_TOKEN_EXPIRATION_SECONDS), // Cookie validity duration in milliseconds
 	});
 
